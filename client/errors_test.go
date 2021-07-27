985cf9240ea137968da46014bbdf490b56ec19d7
Anton Ekberg <anton.ekberg@skf.com>
2021-07-27 09:40:44 +0200

    Add simple client error test

---
+++ client/errors_test.go
@@ -0,0 +1,37 @@
+package client_test
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/SKF/go-rest-utility/client"
+)
+
+func TestClientGet_NotFoundError(t *testing.T) {
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "a nice description on why teapots are bad")
+	}))
+	defer srv.Close()
+
+	request := client.Get("endpoint")
+
+	c := client.NewClient(client.WithBaseURL(srv.URL))
+
+	_, err := c.Do(context.Background(), request)
+	require.Error(t, err)
+
+	require.ErrorIs(t, err, client.ErrNotFound)
+
+	httpErr := client.HTTPError{}
+	require.ErrorAs(t, err, &httpErr)
+
+	require.Equal(t, http.StatusNotFound, httpErr.StatusCode)
+	require.Equal(t, "Not Found", httpErr.Status)
+	require.Equal(t, "a nice description on why teapots are bad", httpErr.Body)
+}
