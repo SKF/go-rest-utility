b2a60e9e31bb3f6f48b3cfb2968420ef61e2251a
Magnus <magnus.arnevall@skf.com>
2022-08-16 11:48:10 +0200

    Check content length

--- client/client.go
+++ client/client.go
@@ -66,7 +66,7 @@ func (c *Client) DoAndUnmarshal(ctx context.Context, r *Request, v interface{}) 
 		return err
 	}
 
-	if response.StatusCode == http.StatusNoContent {
+	if response.StatusCode == http.StatusNoContent || response.ContentLength == 0 {
 		return nil
 	}
 
