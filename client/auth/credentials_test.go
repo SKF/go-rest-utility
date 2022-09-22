6f105d01ab9c91c660748a1fe3b87bda6068eacf
Anton Ekberg <anton.ekberg@skf.com>
2022-09-22 22:06:38 +0200

    Add retries to auth.CredentialsTokenProvider (#33)

--- client/auth/credentials_test.go
+++ client/auth/credentials_test.go
@@ -3,10 +3,12 @@ package auth_test
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 
 	"github.com/SKF/go-rest-utility/client/auth"
+	"github.com/SKF/go-rest-utility/client/retry"
 )
 
 func TestCredentialsTokenProvider_ValidToken(t *testing.T) {
@@ -132,4 +134,111 @@ func TestCredentialsTokenProvider_UnknownTokenType(t *testing.T) {
 	_, err := provider.GetRawToken(context.Background())
 
 	require.ErrorIs(t, err, auth.ErrUnknownTokenType)
+}
+
+func TestCredentialsTokenProvider_IncorrectCredentialsWithRetries(t *testing.T) {
+	t.Parallel()
+
+	sso := NewSSO().WithUser(TestUser{
+		Username: "john.doe@example.com",
+		Password: "correct-password",
+	})
+	defer sso.Close()
+
+	provider := auth.CredentialsTokenProvider{
+		Username: "john.doe@example.com",
+		Password: "incorrect-password",
+		Endpoint: sso.URL,
+		Retry: &retry.ExponentialJitterBackoff{
+			Base:        time.Millisecond,
+			MaxAttempts: 3,
+		},
+	}
+
+	_, err := provider.GetRawToken(context.Background())
+
+	require.ErrorIs(t, err, auth.ErrIncorrectCredentials)
+
+	sso.RequireSignInCalls(t, "john.doe@example.com", 1)
+}
+
+func TestCredentialsTokenProvider_RatelimitedWithRetries(t *testing.T) {
+	t.Parallel()
+
+	sso := NewSSO().WithUser(TestUser{
+		Username:    "john.doe@example.com",
+		Password:    "very-secret",
+		Ratelimited: true,
+	})
+	defer sso.Close()
+
+	provider := auth.CredentialsTokenProvider{
+		Username: "john.doe@example.com",
+		Password: "very-secret",
+		Endpoint: sso.URL,
+		Retry: &retry.ExponentialJitterBackoff{
+			Base:        time.Millisecond,
+			MaxAttempts: 3,
+		},
+	}
+
+	_, err := provider.GetRawToken(context.Background())
+
+	require.ErrorIs(t, err, auth.ErrTooManyRequests)
+
+	sso.RequireSignInCalls(t, "john.doe@example.com", 4)
+}
+
+func TestCredentialsTokenProvider_ChallengedWithRetries(t *testing.T) {
+	t.Parallel()
+
+	sso := NewSSO().WithUser(TestUser{
+		Username:   "john.doe@example.com",
+		Password:   "temporary-password",
+		Challenged: true,
+	})
+	defer sso.Close()
+
+	provider := auth.CredentialsTokenProvider{
+		Username: "john.doe@example.com",
+		Password: "temporary-password",
+		Endpoint: sso.URL,
+		Retry: &retry.ExponentialJitterBackoff{
+			Base:        time.Millisecond,
+			MaxAttempts: 3,
+		},
+	}
+
+	_, err := provider.GetRawToken(context.Background())
+
+	require.ErrorIs(t, err, auth.ErrChallenged)
+
+	sso.RequireSignInCalls(t, "john.doe@example.com", 1)
+}
+
+func TestCredentialsTokenProvider_InactiveWithRetries(t *testing.T) {
+	t.Parallel()
+
+	sso := NewSSO().WithUser(TestUser{
+		Username: "john.doe@example.com",
+		Password: "very-secret",
+		Inactive: true,
+	})
+	defer sso.Close()
+
+	provider := auth.CredentialsTokenProvider{
+		Username: "john.doe@example.com",
+		Password: "very-secret",
+		Endpoint: sso.URL,
+		Retry: &retry.ExponentialJitterBackoff{
+			Base:        time.Millisecond,
+			MaxAttempts: 3,
+		},
+	}
+
+	_, err := provider.GetRawToken(context.Background())
+
+	require.ErrorIs(t, err, auth.ErrInactivated)
+
+	sso.RequireSignInCalls(t, "john.doe@example.com", 1)
 }
